340e30663ddcb962062105c2e9ba4a85745db200
YT98 <yannistarfa98@gmail.com>
2020-10-31 15:18:09 -0400

    Added file tools

---
+++ file_tools.py
@@ -0,0 +1,20 @@
+import os
+
+dirname = os.path.dirname(__file__)
+
+silhouettes = os.path.join(dirname, "data/silouhettes")
+scans = os.path.join(dirname, "data/scanned_documents")
+
+def directory_image_list(directory):
+    list = []
+    for filename in os.listdir(directory):
+        if (os.path.splitext(filename)[1] == ".jpg"):
+            list.append(os.path.join(dirname, directory, filename))
+    print(list)
+    return list
+
+def get_silhouettes():
+    return directory_image_list(silhouettes)
+
+def get_scans():
+    return directory_image_list(scans)
