c536c2417e894bea976f1cb6a941dd0127d78bdf
YT98 <yannistarfa98@gmail.com>
2020-11-01 12:44:29 -0500

    Added gaussian blur function

--- image_tools.py
+++ image_tools.py
@@ -7,15 +7,20 @@ def load_image(path):
     image = cv2.imread(path, 0)
     return asarray(image).copy()
 
-# Shows numpy array as image with pyplot
+# Shows image with pyplot
 def show_image(image):
     plt.imshow(image, cmap="gray")
     plt.show()
 
-# Resizes numpy array keeping its original aspect ratio
+# Resizes image keeping its original aspect ratio
 def resize_image(image, scale_percent):
     width = int(image.shape[1] * scale_percent / 100)
     height = int(image.shape[0] * scale_percent / 100)
     shape = (width, height)
     resized = cv2.resize(image, shape, interpolation=cv2.INTER_AREA)
     return asarray(resized).copy()
+
+# Blur images with openCV Gaussian blur
+def blur_image(image):
+    kernel_size = (81,81)
+    return cv2.GaussianBlur(image, kernel_size, 0)
