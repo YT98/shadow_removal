b780a4252096cd13030c967d5832bc10c95b3e92
YT98 <yannistarfa98@gmail.com>
2020-11-01 22:01:04 -0500

    added rename_files function and path variables, removed get functions for specific directories

--- file_tools.py
+++ file_tools.py
@@ -1,19 +1,30 @@
 import os
 
+# Directory paths
+# ps - phone silhouettes
+# se, he - soft edges, hard edges
+# tb, wb - transparent background, white background
 dirname = os.path.dirname(__file__)
+data = os.path.join(dirname, "data")
+ps_path = os.path.join(dirname, "data/phone_silhouettes")
+ps_he_tb = os.path.join(ps_path, "hard_edges/trans_bg")
+ps_he_wb = os.path.join(ps_path, "hard_edges/white_bg")
+ps_se_tb = os.path.join(ps_path, "soft_edges/trans_bg")
+ps_se_wb = os.path.join(ps_path, "soft_edges/white_bg")
+scanned_documents_path = os.path.join(dirname, "data/scanned_documents/grayscale")
 
-silhouettes = os.path.join(dirname, "data/silouhettes")
-scans = os.path.join(dirname, "data/scanned_documents")
-
-def directory_image_list(directory):
+# Returns list of png or jpg files in directory
+def directory_image_list(directory, ext):
     list = []
     for filename in os.listdir(directory):
-        if (os.path.splitext(filename)[1] == ".jpg"):
+        if (os.path.splitext(filename)[1] == ext):
             list.append(os.path.join(dirname, directory, filename))
     return list
 
-def get_silhouettes():
-    return directory_image_list(silhouettes)
-
-def get_scans():
-    return directory_image_list(scans)
+# Renames all files in directory with name and number
+def rename_files(directory, name):
+    for count, filename in enumerate(os.listdir(directory)):
+        ext = os.path.splitext(filename)[1]
+        src = os.path.join(directory, filename)
+        dst = os.path.join(directory, name + str(count) + ext)
+        os.rename(src, dst)
