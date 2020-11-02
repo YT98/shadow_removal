6a2a38390f3fbfdd00c557b1d5e1d658cf502cb2
YT98 <yannistarfa98@gmail.com>
2020-11-02 16:29:40 -0500

    Added add_noise method to Mask. Removed self.shape attribute. Changed change_transparency function to have a smooth distribution of alpha channel values.

--- Mask.py
+++ Mask.py
@@ -1,21 +1,21 @@
-from image_tools import *
-from file_tools import *
 import cv2
 import random
+from image_tools import *
+from file_tools import *
+from perlin_noise import *
+
 
 class Mask:
     def __init__(self, shadow):
         self.shadow = shadow
-        self.shape = shadow.shape
     
     # Resizes shadow keeping its original aspect ratio
     def scale(self, scale):
-        width = int(self.shape[1] * scale / 100)
-        height = int(self.shape[0] * scale / 100)
+        width = int(self.shadow.shape[1] * scale / 100)
+        height = int(self.shadow.shape[0] * scale / 100)
         shape = (width, height)
         scaled = cv2.resize(self.shadow, shape, interpolation=cv2.INTER_AREA)
         self.shadow = np.asarray(scaled)
-        self.shape = scaled.shape
 
     # Pads shadow with transparent pixels 
     # Returned image is of same shape as input image
@@ -23,7 +23,7 @@ class Mask:
     def pad(self, image_shape):
         # TODO: Catch error if shadow is larger than img
         (img_h, img_w, _) = image_shape
-        (shadow_h, shadow_w, _) = self.shape
+        (shadow_h, shadow_w, _) = self.shadow.shape
         padding_top = img_h - shadow_h
         padding_left = random.randint(0, img_w - shadow_w)
         padding_right = img_w - shadow_w - padding_left
@@ -37,35 +37,62 @@ class Mask:
             value=[255,255,255,0]
         )
         self.shadow = padded
-        self.shape = padded.shape
 
     # Changes mask transparency
     def change_transparency(self, transparency):
         new_shadow = self.shadow.copy()
-        a_channel_value = int(255*transparency)
-        for x in range(new_shadow.shape[0]):
-            for y in range(new_shadow.shape[1]):
-                if (new_shadow[x, y, 3] != 0):
-                    new_shadow[x, y, 3] = a_channel_value
+        new_a = new_shadow[:,:,3]
+        new_a = np.array(new_a * transparency, dtype=np.uint8)
+        r, g, b, old_a = cv2.split(new_shadow)
+        new_shadow = cv2.merge((r,g,b,new_a))
         self.shadow = new_shadow
 
     # Applies mask to given image
     def apply(self, image):
+        self.pad(image.shape)
         new_image = image.copy()
         # Normalize alpha channel from 0-255 to 0-1
         alpha = self.shadow[:,:,3] / 255.0
+        # TODO try doing :3 instead of range(3)
         # Element-wise multiplication, each channel multiplied by alpha (or 1-alpha)
         for color in range(3):
             new_image[:,:,color] = alpha * self.shadow[:,:,color] + (1-alpha) * new_image[:,:,color]
         return new_image
 
-    # Applies blurring to have a more realistic shadow
+    # Applies gaussian blur to have a more realistic shadow
     def blur(self):
         # Pad shadow a little
-        self.pad((self.shape[0]+50, self.shape[1]+50, 0))
+        self.pad((self.shadow.shape[0]+50, self.shadow.shape[1]+50, 0))
         # Gaussian blur
         kernel_size = (15, 15)
         blurred = cv2.GaussianBlur(self.shadow, kernel_size, 15)
         self.shadow = blurred
         
+    # Adds gaussian noise to have a more realistic shadow
+    def add_noise(self):
+        # Pad shadow a little
+        self.pad((self.shadow.shape[0]+50, self.shadow.shape[1]+50, 0))
+        # Create perlin mask
+        scale = 100.0
+        octaves = 4
+        lacunarity = 3.0
+        base = 1
+        persistence = random.uniform(0.05, 0.25)
+        mask = perlin_mask(
+            (self.shadow.shape[0], self.shadow.shape[1]), 
+            scale, 
+            octaves, 
+            persistence, 
+            lacunarity, 
+            base
+        )
+        # Add alpha channel to perlin mask
+        new_mask = mask.copy()
+        a = self.shadow[:,:,3]
+        r,g,b = cv2.split(new_mask)
+        new_mask = cv2.merge((r,g,b,a))
+        self.perlin_mask = new_mask
+        self.shadow = self.apply(new_mask)
+
+
 
