14b58cd0ec8c195f7bc82d82ebffd1f76e3dce20
YT98 <yannistarfa98@gmail.com>
2020-11-02 16:28:38 -0500

    Created perlin noise mask function

---
+++ perlin_noise.py
@@ -0,0 +1,26 @@
+import cv2
+import noise
+import numpy as np
+from image_tools import show_image
+import random
+
+def perlin_mask(shape, scale, octaves, persistence, lacunarity, base):
+    mask = np.zeros(shape)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            # Value is normalized between -1 and 1
+            pixel = noise.pnoise2(i/scale, 
+                                        j/scale, 
+                                        octaves=octaves, 
+                                        persistence=persistence, 
+                                        lacunarity=lacunarity, 
+                                        repeatx=1024, 
+                                        repeaty=1024, 
+                                        base=base)
+            # Denormalize between 0 and 255
+            mask[i][j] = ((pixel+1)/2.0*255.0)
+    # Cast float64 array to uint8
+    mask = mask.astype(np.uint8)
+    # Converting to RGB
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+    return mask
