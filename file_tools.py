d1f1466b3e048181ffdf4687a3e67454b2f28c73
YT98 <yannistarfa98@gmail.com>
2020-11-03 15:26:08 -0500

    Added mask directory paths and save_image method

--- file_tools.py
+++ file_tools.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 
 # Directory paths
 # ps - phone silhouettes
@@ -12,6 +13,8 @@ ps_he_wb = os.path.join(ps_path, "hard_edges/white_bg")
 ps_se_tb = os.path.join(ps_path, "soft_edges/trans_bg")
 ps_se_wb = os.path.join(ps_path, "soft_edges/white_bg")
 scanned_documents_path = os.path.join(dirname, "data/scanned_documents/grayscale")
+perlin_masks_path = os.path.join(data, "masks/perlin_masks")
+silhouette_masks_path = os.path.join(data, "masks/silhouette_masks")
 
 # Returns list of png or jpg files in directory
 def directory_image_list(directory):
@@ -28,3 +31,8 @@ def rename_files(directory, name):
         src = os.path.join(directory, filename)
         dst = os.path.join(directory, name + str(count) + ext)
         os.rename(src, dst)
+
+# Saves image to destination
+def save_image(destination, image):
+    print(image)
+    cv2.imwrite(destination, image)
