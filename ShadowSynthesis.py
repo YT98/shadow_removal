2f985f4cdd294d5fc8481a3cde8cd83c218d0f47
YT98 <yannistarfa98@gmail.com>
2020-11-05 00:08:48 -0500

    Added batches to ShadowSynthesis create_training_data()

--- ShadowSynthesis.py
+++ ShadowSynthesis.py
@@ -13,24 +13,39 @@ class ShadowSynthesis:
         self.mask_set = MaskSet(self.document_set.largest_shape, load=load)
         self.training_data = {}
 
-    def create_training_data(self):
+
+
+    def create_training_data(self, batch_count):
         document_set = self.document_set.document_set
         mask_set = self.mask_set.mask_set
-        training_data_length = len(document_set) * len(mask_set)
-        # Start timer
-        start = time.time()
-        # Initialize progress bar
-        print_progress_bar(start, 0, training_data_length, prefix="Masked images created", suffix="Complete", length=50)
-        count = 0
-        # Create and save masked images
-        for doc_index, doc in enumerate(self.document_set.document_set):
-            document_destination = os.path.join(self.training_data_path, "document_" + str(doc_index) + ".jpg")
-            doc = cv2.cvtColor(doc, cv2.COLOR_GRAY2RGB)
-            file_tools.save_image(document_destination, doc)
-            for mask_index, mask in enumerate(self.mask_set.mask_set):
-                masked_document_destination = os.path.join(self.training_data_path, "document_" + str(doc_index) + "_mask_" + str(mask_index) + ".jpg")
-                masked_doc = mask.apply(doc)
-                file_tools.save_image(masked_document_destination, masked_doc)
-                # Update progress bar
-                count += 1
-                print_progress_bar(start, count, training_data_length, prefix="Masked images created", suffix="Complete", length=50)
+
+        # Create document image batches
+        batch_length = len(document_set) // batch_count
+        batch_set = []
+        for batch_index in range(batch_count - 1):
+            batch_start = batch_index * batch_length
+            batch_end = (batch_index + 1) * batch_length
+            batch_set = self.document_set.document_set[batch_start:batch_end]
+
+        progress_bar_length = batch_length * len(mask_set)
+
+        # Iterate over batches
+        for batch_index, batch in enumerate(batch_set):
+            # Start timer
+            start = time.time()
+            # Initialize progress bar
+            print_progress_bar(start, 0, progress_bar_length, prefix="Masked images created - batch {}".format(batch_index+1), suffix="Complete", length=50)
+            count = 0
+            # Create and save masked images
+            for doc_batch_index, doc in enumerate(batch):
+                doc_index = (batch_index * doc_batch_index) + doc_batch_index
+                document_destination = os.path.join(self.training_data_path, "document_" + str(doc_index) + ".jpg")
+                doc = cv2.cvtColor(doc, cv2.COLOR_GRAY2RGB)
+                file_tools.save_image(document_destination, doc)
+                for mask_index, mask in enumerate(self.mask_set.mask_set):
+                    masked_document_destination = os.path.join(self.training_data_path, "document_" + str(doc_index) + "_mask_" + str(mask_index) + ".jpg")
+                    masked_doc = mask.apply(doc)
+                    file_tools.save_image(masked_document_destination, masked_doc)
+                    # Update progress bar
+                    count += 1
+                    print_progress_bar(start, count, progress_bar_length, prefix="Masked images created - batch {}".format(batch_index+1), suffix="Complete", length=50)
