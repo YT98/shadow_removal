2b534faeffd0742dbd6f4087723cbc94f134c22e
YT98 <yannistarfa98@gmail.com>
2020-11-08 12:22:29 -0500

    Updated path variables

--- tools/file_tools.py
+++ tools/file_tools.py
@@ -2,20 +2,25 @@ import os
 import cv2
 
 # Directory paths
+dirname = os.path.join(os.path.dirname(__file__), "../")
+data = os.path.join(dirname, "data")
+# Mask paths
 # ps - phone silhouettes
 # se, he - soft edges, hard edges
 # tb, wb - transparent background, white background
-dirname = os.path.join(os.path.dirname(__file__), "../")
-data = os.path.join(dirname, "data")
 ps_path = os.path.join(dirname, "data/phone_silhouettes")
 ps_he_tb = os.path.join(ps_path, "hard_edges/trans_bg")
 ps_he_wb = os.path.join(ps_path, "hard_edges/white_bg")
 ps_se_tb = os.path.join(ps_path, "soft_edges/trans_bg")
 ps_se_wb = os.path.join(ps_path, "soft_edges/white_bg")
-scanned_documents_path = os.path.join(dirname, "data/scanned_documents/grayscale")
 perlin_masks_path = os.path.join(data, "masks/perlin_masks")
 silhouette_masks_path = os.path.join(data, "masks/silhouette_masks")
-smart_doc_images_path = "/Volumes/UNTITLED/SmartDoc/Dataset/Captured_Images/Samsung_Phone/Images"
+# Document paths
+scanned_documents_path = os.path.join(data, "scanned_documents/grayscale")
+# hard_drive_path = "/Volumes/UNTITLED/"
+# smart_doc_documents_path = os.path.join(hard_drive_path, "smart_doc_documents")
+smart_doc_documents_path = os.path.join(data, "smart_doc_documents")
+# Training data paths
 training_data_path = os.path.join(data, "training_data")
 
 # Returns list of png or jpg files in directory
