704055d88894f0d88abb1e91d703c0c3e6d2b457
YT98 <yannistarfa98@gmail.com>
2020-11-08 08:05:03 -0500

    Created new SIlhouetteMask and MaskSet classes

---
+++ shadow_synthesis2/MaskSet.py
@@ -0,0 +1,37 @@
+import os
+import random
+import multiprocessing
+
+import tools.file_tools as file_tools
+
+from shadow_synthesis2.SilhouetteMask import SilhouetteMask
+
+
+class MaskSet:
+    def __init__(self, largest_shape, load=False):
+        self.largest_shape = largest_shape
+        self.silhouettes_path = file_tools.ps_he_tb
+        self.silhouette_mask_path = file_tools.silhouette_masks_path
+        if not load:
+            self.create_silhouette_masks()
+
+    def create_silhouette_mask(self, silhouette_path):
+        mask = SilhouetteMask(silhouette_path=silhouette_path)
+        mask.add_noise()
+        mask.blur()
+        mask.scale(random.randint(200, 500))
+        mask.change_transparency(random.uniform(0.4, 0.7))
+        mask.save_mask()
+
+    def create_silhouette_masks(self):
+        print("Creating silhouette masks...")
+        # Get silhouette paths
+        silhouette_paths = file_tools.directory_image_list(self.silhouettes_path)
+        # Delete already existing masks from directory
+        for f in os.listdir(self.silhouette_mask_path):
+            os.remove(os.path.join(self.silhouette_mask_path, f))
+        # Create silhouette masks
+        pool = multiprocessing.Pool()
+        pool.map(self.create_silhouette_mask, silhouette_paths)
+        pool.close()
+        print("Silhouette masks created.")
